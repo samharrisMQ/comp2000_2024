4826bbdff31ef8ff35ce7d45d0c38d1c4744d52c
samharrisMQ <samuel.harris2@students.mq.edu.au>
2024-08-13 17:49:48 +1000

    finished task 5 thank fuck for void run

--- Cell.java
+++ Cell.java
@@ -5,6 +5,7 @@ public class Cell {
     //fields
     int x;
     int y;
+    static int size = 35;
 
     //constructors
     public Cell(int x, int y) {
@@ -13,10 +14,22 @@ public class Cell {
     }
 
     //methods
-    void paint(Graphics g) {
-        g.setColor(Color.WHITE);
-        g.fillRect(x,y,35,35);
+    void paint(Graphics g, Point mousePos) {
+        if(contains(mousePos)) {
+            g.setColor(Color.GRAY);
+        } else {
+            g.setColor(Color.WHITE);
+        }
+        g.fillRect(x,y,size,size);
         g.setColor(Color.BLACK);
-        g.drawRect(x,y,35,35);
+        g.drawRect(x,y,size,size);
+    }
+
+    boolean contains(Point p) {
+        if(p!=null) {
+            return (x < p.x && x+size > p.x && y < p.y && y+size > p.y);
+        } else {
+            return false;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,20 @@ public class Main extends JFrame {
         Grid grid;
 
         public Canvas() {
-            grid = new Grid();
             setPreferredSize(new Dimension(720,720));
+            grid = new Grid();
         }
 
         @Override
         public void paint(Graphics g) {
-            System.out.println(getMousePosition());
-            grid.paint(g);
+            grid.paint(g, getMousePosition());
         }
+
     }
 
     public static void main(String[] args) throws Exception {
         Main window = new Main();
+        window.run();
     }
 
     private Main() {
@@ -29,5 +30,11 @@ public class Main extends JFrame {
         this.setContentPane(canvas);
         this.pack();
         this.setVisible(true);
+    }
+
+    public void run() {
+        while (true) {
+            this.repaint();
+        }
     }
 }
